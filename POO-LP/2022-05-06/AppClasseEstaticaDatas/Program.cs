d377e2d3499bc0d3ef7103389426a1938f9f2567
Gabriel R. Antunes <43099880+guesant@users.noreply.github.com>
2022-05-09 08:26:45 -0400

    2022-05-06: AppClasseEstaticaDias

---
+++ POO-LP/2022-05-06/AppClasseEstaticaDatas/Program.cs
@@ -0,0 +1,33 @@
+﻿using AppClasseEstaticaDatas.RegrasDeNegocio;
+
+/*
+DateTime dataNascimento = new DateTime(2005, 09, 13);
+
+DateTime dataAlvo = DateTime.Today;
+
+TimeSpan diferenca = dataAlvo - dataNascimento;
+
+int dias = diferenca.Days;
+
+int meses = (int)(diferenca.Days / 30.436875);
+
+int anos = (int)(diferenca.Days / 365.2425);
+
+Console.WriteLine($"Dias de Vida: {dias}.");
+Console.WriteLine($"Meses de Vida: {meses}.");
+Console.WriteLine($"Anos de Vida: {anos}.");
+*/
+
+Console.Write("Data de Nascimento: (DD/MM/AAAA)...: ");
+string dataNasc = Console.ReadLine()!;
+
+Console.Write("Data Alvo (deixe em branco para pegar a data atual): (DD/MM/AAAA)...: ");
+string dataAlvo = Console.ReadLine()!;
+
+int idade = MDatas.CalcularIdade(dataNasc, dataAlvo);
+int meses = MDatas.CalcularMesesDeVida(dataNasc, dataAlvo);
+int dias = MDatas.CalcularDiasDeVida(dataNasc, dataAlvo);
+
+Console.WriteLine($"- Idade: {idade}.");
+Console.WriteLine($"- Meses: {meses}.");
+Console.WriteLine($"-  Dias: {dias}.");
